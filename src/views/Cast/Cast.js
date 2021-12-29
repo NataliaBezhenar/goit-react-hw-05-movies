d062592ac5e685c570e8629e0ab0fa60e0873497
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2021-12-29 21:39:46 +0200

    styles added

--- src/views/Cast/Cast.js
+++ src/views/Cast/Cast.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import * as moviesAPI from "../../services/movies-api";
+import styles from "./Cast.module.css";
 
 export default function Cast({ movieId }) {
   const [cast, setCast] = useState(null);
@@ -14,12 +15,17 @@ export default function Cast({ movieId }) {
 
   return (
     <div>
-      <ul>
+      <ul className={styles.cardSet}>
         {cast &&
           cast.cast.map(({ id, profile_path, original_name, character }) => (
-            <li key={id}>
+            <li key={id} className={styles.cardItem}>
               <img
-                src={`https://image.tmdb.org/t/p/w300${profile_path}`}
+                className={styles.actor_img}
+                src={
+                  profile_path !== null
+                    ? `https://image.tmdb.org/t/p/w300${profile_path}`
+                    : "https://cdn.pixabay.com/photo/2021/07/04/20/10/owl-6387500_960_720.png"
+                }
                 alt={original_name}
               />
               <div>
--- src/views/MovieCard/MovieCard.js
+++ src/views/MovieCard/MovieCard.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import { useState, useEffect, lazy, Suspense } from "react";
 import styles from "./MovieCard.module.css";
+import navStyles from "../../components/Navigation/Navigation.module.css";
 import * as moviesAPI from "../../services/movies-api";
 const Cast = lazy(() => import("../Cast/Cast.js"));
 const Reviews = lazy(() => import("../Reviews/Reviews.js"));
@@ -32,7 +33,11 @@ export default function MovieCard() {
           <div className={styles.shortInfo}>
             <div>
               <img
-                src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
+                src={
+                  movie.poster_path !== null
+                    ? `https://image.tmdb.org/t/p/w400${movie.poster_path}`
+                    : "https://cdn.pixabay.com/photo/2016/11/03/14/18/stamp-1794352_960_720.png"
+                }
                 alt={movie.title}
               />
             </div>
@@ -49,12 +54,26 @@ export default function MovieCard() {
           </div>
           <div>
             <h3>Additional Information</h3>
-            <ul>
+            <ul className={styles.additionalInfoSet}>
               <li>
-                <NavLink to={{ pathname: "cast" }}>Cast</NavLink>
+                <NavLink
+                  to={{ pathname: "cast" }}
+                  className={(navData) =>
+                    navData.isActive ? navStyles.activeLink : navStyles.link
+                  }
+                >
+                  Cast
+                </NavLink>
               </li>
               <li>
-                <NavLink to={{ pathname: "reviews" }}>Reviews</NavLink>
+                <NavLink
+                  to={{ pathname: "reviews" }}
+                  className={(navData) =>
+                    navData.isActive ? navStyles.activeLink : navStyles.link
+                  }
+                >
+                  Reviews
+                </NavLink>
               </li>
             </ul>
             <Suspense fallback={<h1>Loading.....................</h1>}>
